8e8513bf4d375312566e7f3139fd9ab5067e41d1
l-robison <32803122+l-robison@users.noreply.github.com>
2017-10-21 20:30:07 -0400

    Added card def

--- spade/src/main.rs
+++ spade/src/main.rs
@@ -1,3 +1,19 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Nicon!");
+
+    let x : Card = Card(Suit::Club, Value::Three);
+
+    println!("{:?}",x);
 }
+
+#[derive(Debug)]
+enum Suit { Club, Diamond, Heart, Spade }
+
+#[derive(Debug)]
+enum Value {Two,Three,Four,Five,Six,Seven,Eight,Nine,Ten,Jack,Queen,King,Ace}
+
+
+#[derive(Debug)]
+struct Card(Suit, Value);
